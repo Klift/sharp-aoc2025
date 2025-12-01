a8acc64da473cc60d52ffc7a1f6f3bea4719c73c
klift <skipeklift@gmail.com>
2025-12-01 23:08:20 +0100

    Day01 Solution for part 1 and 2

---
+++ AoC.Solutions/Day01.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC.Core;
+
+namespace AoC.Solutions
+{
+    public class Day01 : ISolver
+    {
+        public uint Day => 1u;
+
+        public string Title => "Secret Entrance";
+
+        public object Part1(string input)
+        {
+            var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            int currentDial = 50;
+            int result = 0;
+
+            foreach (var line in lines)
+            {
+                
+                int rotateBy = int.Parse(line.Substring(1));
+                currentDial += (line.StartsWith('L') ? -1 : +1) * rotateBy;
+
+                if (currentDial % 100 == 0)
+                {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+
+        public object Part2(string input)
+        {
+            var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            long currentDial = 50;
+            long result = 0;
+
+            foreach (var line in lines)
+            {
+                int rotateBy = int.Parse(line.Trim().Substring(1));
+                int direction = line.Trim().StartsWith('L') ? -1 : 1;
+
+                for (int i = 0; i < rotateBy; i++)
+                {
+                    currentDial += direction;
+
+                    if (currentDial % 100 == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AoC.Tests/Day01Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC.Solutions;
+using FluentAssertions;
+
+namespace AoC.Tests
+{
+    public class Day01Tests : SolverTestBase<Day01>
+    {
+        protected override string SampleInput => """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82
+            """;
+
+        [Fact]
+        public void Part1_ShouldMatchTheSample()
+        {
+            var result = _sut.Part1(SampleInput);
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Part2_ShouldMatchTheSample()
+        {
+            _sut.Part2(SampleInput).Should().Be(6);
+        }
+
+        [Theory]
+        [InlineData("R200", 2)] // Multiple Full Positive Rotations
+        [InlineData("R150", 2)] // Stopping at 0
+        [InlineData("L75", 1)] // Sign Change non-complete
+        [InlineData("L200", 2)] // Sign Change multiple complete
+        [InlineData("L150", 2)] // Sign Change stopping at 0
+        [InlineData("L50\nL150\nR200", 4)] // changing signs with more complex rotation
+        public void Part2_EdgeCases(string input, int expected)
+        {
+            // Act
+            var result = _sut.Part2(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
